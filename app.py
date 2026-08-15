1174582306adc650d7832796320e9310abb1e393
deivisondaniel-cpu <deivison.daniel@losungexpress.com.br>
2026-08-15 16:43:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 # ============================================================
 st.set_page_config(
     page_title="Monitoramento Amazon",
-    page_icon="📦",
+    page_icon="🕒",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
